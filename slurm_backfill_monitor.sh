df6fe1eff1155929590c5d96071f56c4a2f1fb2f
the-germanator <cvs46@nau.edu>
2019-03-15 12:38:25 -0700

    Version 2
    Enables debugging for specific amount of time and then checks if backfill occurred in same amount of time.

--- slurm_backfill_monitor.sh
+++ slurm_backfill_monitor.sh
@@ -1,40 +1,73 @@
 #!/bin/bash
-#
-# Author: Christoph Schild <cvs46@nau.edu>
-# This script monitors the backfill log in Slurm and uses return codes to communicate its state.
-# Parses slurm.conf is used to determine if backfilling is enabled. 
-# NOTE: Must be run as a user with access to slurm.conf and /var/log/slurm
-#
-# Determine if Backfill is enabled
-if [[ $(cat /etc/slurm/slurm.conf | grep DebugFlags=Backfill) == "#"* ]]; then
-   # Line is commented out, use return code 2
-   exit 2
-else
-   # Line is NOT commented out
-   # Get Date of last backfill complete (DEBUGGING IN THIS LINE)
-   OLDDATE=$(tac /var/log/slurm/slurmctld.log | grep 'backfill: reached end of job queue' -m 1| cut -d '.' -f 1 | cut -d '[' -f 2 | tr -s 'T' ' ')
-   # Return exit code 2 if NO backfills have completed
-   if [[ -z $OLDDATE ]]; then
-      exit 2
-   fi   
-   # Get timestamp (unix epoch) for time in log
-   OLDRAW=$(date -d "$OLDDATE" +"%s")
-   
-   # Get timestamp (unix epoch) for current time
-   NEW=$(date +"%s")
 
-   # Use time passed as parameter to verify timeperiod
-   TIMEDIFFALLOWED=$1
-   
-   # Subtract old from new to get time differential
-   SUB=$(($NEW - $OLDRAW))
-   
-   # Check if time differential falls in provided range
-   if [[ $SUB -le $TIMEDIFFALLOWED  ]]; then
-      # It does, all is well. Return 0
-      exit 0
-   else
-      # It does not, return exit code 1
-      exit 1
-   fi
+# Author: Chritoph Schild <cvs46@nau.edu>
+# This program enables the semi-efficient monitoring of the Slurm backfill log
+# by enabling the "DebugFlags=Backfill" parameter in slurm.conf and then disabling it
+# a predetermined amount of time. During the time it's enabled, we check if the backfill has occured.
+
+# Time to collect sample (in seconds)
+SAMPLETIME=30
+
+# Slurm config file
+SLURMCONFIG="/home/cvs46/backfill_v2/slurm.conf"
+
+# Slurmctld log
+SLURMCTLDLOG="/var/log/slurm/slurmctld.log"
+
+# Comment to include in config file
+CONFCOMMENT="# Edited by Backfill Monitor"
+
+# Log File
+LOGFILE="/home/cvs46/backfill_log.txt"
+
+# All good action
+SUCCESS="echo All good"
+
+# Backfill issue
+FAILURE="echo There was an issue."
+
+
+################# Enable Debugging, Wait, Disable and Listen #############################
+
+# Uncomment backfill line
+sudo sed -i 's/#DebugFlags=Backfill/DebugFlags=Backfill/' $SLURMCONFIG
+
+# Kill process
+kill -1 $(pgrep slurmctld)
+
+# Sleep
+sleep $SAMPLETIME
+
+# Reinstate comment
+sudo sed -i 's/DebugFlags=Backfill/#DebugFlags=Backfill/' $SLURMCONFIG
+
+
+############ Get data from logs regarding backfill ######################################
+
+# Get date of last backfill
+OLDDATE=$(sudo tac /var/log/slurm/slurmctld.log | grep 'backfill: reached end of job queue' -m 1| cut -d '.' -f 1 | cut -d '[' -f 2 | tr -s 'T' ' ')
+
+# Check if date is blank (no backfill records) exit with status 2
+if [[ -z $OLDDATE ]]; then
+   exit 2
+fi 
+
+# If you ar here, there was a backfill at /Some/ point
+
+# Get timestamp (unix epoch) for time in log
+OLDTIMERAW=$(date -d "$OLDDATE" +"%s")
+
+# Get timestamp (unix epoch) for current time
+NEWTIMERAW=$(date +"%s")
+
+# Subtract old from new to get time differential
+TIMEDIFFERENCE=$(($NEWTIMERAW - $OLDTIMERAW))
+
+# Check if time differential falls in provided range
+if [[ $TIMEDIFFERENCE -le $SAMPLETIME  ]]; then
+   # It does, all is well. Return 0
+   exit 0
+else
+   # It does not, return exit code 1
+   exit 1
 fi
